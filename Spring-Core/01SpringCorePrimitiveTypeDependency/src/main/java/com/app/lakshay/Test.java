dfc1013f18fec54165633adf8df2f5ef39a353e6
Lakshay Kumawat <work.lakshayk@gmail.com>
2025-10-07 10:33:33 +0530

    Add files via upload

---
+++ Spring-Core/01SpringCorePrimitiveTypeDependency/src/main/java/com/app/lakshay/Test.java
@@ -0,0 +1,13 @@
+package com.app.lakshay;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+        Student student = (Student) context.getBean("student");
+        System.out.println(student);
+    }
+}
