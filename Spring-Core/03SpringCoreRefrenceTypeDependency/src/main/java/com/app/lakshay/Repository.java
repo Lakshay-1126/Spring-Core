52a0e8a6b021f9f893b007255c574b6d2039ea93
Lakshay Kumawat <work.lakshayk@gmail.com>
2025-10-08 21:25:55 +0530

    Add files via upload

---
+++ Spring-Core/03SpringCoreRefrenceTypeDependency/src/main/java/com/app/lakshay/Repository.java
@@ -0,0 +1,27 @@
+package com.app.lakshay;
+
+public class Repository {
+
+	private String entity;
+
+	public Repository() {
+		super();
+	}
+
+	public String getEntity() {
+		return entity;
+	}
+
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+
+	@Override
+	public String toString() {
+		return "Repository [entity=" + entity + "]";
+	}
+
+	
+	
+	
+}
