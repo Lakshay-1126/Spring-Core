e58e6bc843bc3877bd3a60e9447fccb99c8810c0
Lakshay Kumawat <work.lakshayk@gmail.com>
2025-10-09 23:24:58 +0530

    Add files via upload

---
+++ Spring-Core/03SpringCoreRefrenceTypeDependency-01/src/main/java/com/app/lakshay/Custom.java
@@ -0,0 +1,24 @@
+package com.app.lakshay;
+
+public class Custom {
+
+	private Controller c1;
+
+	public Custom() {
+		super();
+	}
+
+	public Controller getC1() {
+		return c1;
+	}
+
+	public void setC1(Controller c1) {
+		this.c1 = c1;
+	}
+
+	@Override
+	public String toString() {
+		return "Custom [c1=" + c1 + "]";
+	}
+	
+}
