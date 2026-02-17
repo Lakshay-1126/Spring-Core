5ee7c8a980fc12264eec1849268d6065f1bc9f73
Lakshay Kumawat <work.lakshayk@gmail.com>
2026-02-17 13:28:38 +0530

    Add files via upload

---
+++ Spring-Core/09SpringCorePropertiesFile/src/main/java/com/app/MyAppConfig.java
@@ -0,0 +1,22 @@
+package com.app;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+//Generally we use @ComponentScan to give the base package
+@ComponentScan("com.app")
+// That means all your classes present inside this package annotation based
+// their itself
+@PropertySource("classpath:myapp.properties")
+// I want to tell  go to src/main/resource (class path meaning) and the file name is 
+// myapp.properties and load
+
+//1. First annotation will try to find the classes from the current package com.app
+// app package and sub package 
+
+//2. Second annotation will try to load the properties file
+// Annotation you can write in any order (Annotation ever follow order over a class)
+//Now let us try to read the data
+public class MyAppConfig {
+
+}
