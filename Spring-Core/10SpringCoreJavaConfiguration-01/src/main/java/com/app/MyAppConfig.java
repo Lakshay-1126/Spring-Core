453b76d1b8c5cac351cc7b600a6311bc365b9fa9
Lakshay Kumawat <work.lakshayk@gmail.com>
2026-02-17 13:54:48 +0530

    Add files via upload

---
+++ Spring-Core/10SpringCoreJavaConfiguration-01/src/main/java/com/app/MyAppConfig.java
@@ -0,0 +1,20 @@
+package com.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyAppConfig {
+	
+	// 1 Method = 1 Object
+	@Bean
+	public DriverManager conobj() {
+		
+		DriverManager dm = new DriverManager();
+		dm.setDriverClass("ORACLE");
+		dm.setUrl("JDBC:ORACLE");
+		
+		return dm;
+	}
+
+}
